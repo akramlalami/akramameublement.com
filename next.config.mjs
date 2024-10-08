82c5eb05e380b8282e5a9615f949cb2d97b2edf3
akramlalami <khalilouxx@gmail.com>
2024-10-08 13:04:01 +0200

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            '127.0.0.1',
+            'akramapi.akramameublement.com' 
+           
+        ],
+    },
+    output: 'standalone',
+};
+
+export default nextConfig;
